37ea9482ee73640530c85531d29e0882a1f81ccc
Lexa622 <lexa622@gmail.com>
2024-10-27 20:01:22 +0300

    Домашка "Использование БД в маршрутизации. Часть 2."

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from app.backend.db_depends import get_db
 # Аннотации, Модели БД и Pydantic.
 from typing import Annotated
-from app.models import User
+from app.models import User, Task
 from app.schemas import CreateUser, UpdateUser
 # Функции работы с записями.
 from sqlalchemy import insert, select, update, delete
@@ -26,6 +26,14 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
     return user
+
+
+@router.get("/user_id/tasks")
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    tasks = db.scalars(select(Task).where(User.id == user_id)).all()
+    if tasks is None:   # бесполезная запись
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tasks was not found")
+    return tasks
 
 
 @router.post("/create")
@@ -57,5 +65,6 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
     db.execute(delete(User).where(User.id == user_id))
+    db.execute(delete(Task).where(Task.user_id == user_id))
     db.commit()
     return {'status_code': status.HTTP_200_OK, 'transaction': 'User delete is successful!'}
